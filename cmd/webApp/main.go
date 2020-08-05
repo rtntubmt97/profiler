21ea46898b037c928546b597458928ba941d2da6
rtntubmt97 <rtntubmt97@gmail.com>
2020-08-05 15:43:04 +0700

    ignore internal, cmd, test directory

--- cmd/webApp/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/rtntubmt97/profiler/internal/endpoints"
-
-func main() {
-	endpoints.StartGinServer()
-}
--- internal/defines/respDescription.go
+++
@@ -1,7 +0,0 @@
-package defines
-
-const Success = "Success"
-const HandleFailed = "Handle failed"
-const DbError = "Database error"
-const NotExist = "Not exist"
-const InvalidParam = "Invalid param"
