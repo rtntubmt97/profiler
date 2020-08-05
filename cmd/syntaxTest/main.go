db44e8a4ff10caa5218ff60164d2c4fabcbfbc8b
rtntubmt97 <rtntubmt97@gmail.com>
2020-08-05 09:42:28 +0700

    add test for internal pkg

--- cmd/syntaxTest/main.go
+++ cmd/syntaxTest/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Mark int64
 
@@ -9,9 +12,15 @@ func proc(mark Mark) {
 }
 
 func main() {
-	var mark Mark
-	mark = 64
-	mark2 := Mark(2)
-	proc(mark)
-	proc(mark2)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":9080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Header.Get("X-Forwarded-For"))
+	fmt.Println(r.Header.Get("x-forwarded-for"))
+	fmt.Println(r.Header.Get("X-FORWARDED-FOR"))
+	fmt.Println(r.Header.Get("Remote_Addr"))
+	fmt.Println(r.RemoteAddr)
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
